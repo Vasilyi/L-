cbc1dd4e7324585d6430fa170d758fa490024560
trus <trus888@gmail.com>
2014-08-20 15:38:40 +0400

    everything should be fixed now

---
+++ IHateSkillshots/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using LeagueSharp;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+
+namespace Skillshots
+{
+    class Program
+    {
+        public static List<Spell> SpellList = new List<Spell>();
+        public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
+        public static Menu Config;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            try
+            {
+                Config = new Menu("Skillshots", "Skillshots", true);
+                Config.AddSubMenu(new Menu("Combo", "Combo"));
+                Config.SubMenu("Combo").AddItem(new MenuItem("Hitchance", "Only High HitChance").SetValue(true));
+                Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+                var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+                SimpleTs.AddToMenu(targetSelectorMenu);
+                Config.AddSubMenu(targetSelectorMenu);
+                foreach (var spell in SpellDatabase.Spells)
+                    if (spell.BaseSkinName == ObjectManager.Player.BaseSkinName)
+                    {
+                        Game.PrintChat(spell.Slot + " LOADED");
+                        if (spell.Slot == SpellSlot.Q)
+                        {
+                            Q = new Spell(spell.Slot, spell.Range);
+                            Q.SetSkillshot(spell.Delay / 1000, spell.Radius, spell.MissileSpeed, spell.CanBeRemoved, spell.Type);
+                            Config.SubMenu("Combo").AddItem(new MenuItem("Spell1", "Q").SetValue(new KeyBind(90, KeyBindType.Press)));
+                            Config.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+                            SpellList.Add(Q);
+                        }
+                        if (spell.Slot == SpellSlot.W)
+                        {
+                            W = new Spell(spell.Slot, spell.Range);
+                            W.SetSkillshot(spell.Delay / 1000, spell.Radius, spell.MissileSpeed, spell.CanBeRemoved, spell.Type);
+                            Config.SubMenu("Drawings").AddItem(new MenuItem("WRange", "W range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+                            Config.SubMenu("Combo").AddItem(new MenuItem("Spell2", "W").SetValue(new KeyBind(88, KeyBindType.Press)));
+                            SpellList.Add(W);
+                        }
+                        if (spell.Slot == SpellSlot.E)
+                        {
+                            E = new Spell(spell.Slot, spell.Range);
+                            E.SetSkillshot(spell.Delay / 1000, spell.Radius, spell.MissileSpeed, spell.CanBeRemoved, spell.Type);
+                            Config.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+                            Config.SubMenu("Combo").AddItem(new MenuItem("Spell3", "E").SetValue(new KeyBind(67, KeyBindType.Press)));
+                            SpellList.Add(E);
+                        }
+                        if (spell.Slot == SpellSlot.R)
+                        {
+                            R = new Spell(spell.Slot, spell.Range);
+                            R.SetSkillshot(spell.Delay / 1000, spell.Radius, spell.MissileSpeed, spell.CanBeRemoved, spell.Type);
+                            Config.SubMenu("Drawings").AddItem(new MenuItem("RRange", "R range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+                            Config.SubMenu("Combo").AddItem(new MenuItem("Spell4", "R").SetValue(new KeyBind(86, KeyBindType.Press)));
+                            SpellList.Add(R);
+                        }
+
+                    }
+            }
+            catch (Exception)
+            {
+                Game.PrintChat("Error found in skillshots. Refused to load.");
+            }
+            Config.AddToMainMenu();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (SpellList == null) return;
+
+            foreach (Spell spell in SpellList)
+            {
+                var menuItem = Config.Item(spell.Slot + "Range").GetValue<Circle>();
+
+                if (menuItem.Active)
+                    Utility.DrawCircle(ObjectManager.Player.Position, spell.Range, menuItem.Color);
+            }
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("Spell1") != null && Config.Item("Spell1").GetValue<KeyBind>().Active)
+                ExecuteQ();
+            if (Config.Item("Spell2") != null && Config.Item("Spell2").GetValue<KeyBind>().Active)
+                ExecuteW();
+            if (Config.Item("Spell3") != null && Config.Item("Spell3").GetValue<KeyBind>().Active)
+                ExecuteE();
+            if (Config.Item("Spell4") != null && Config.Item("Spell4").GetValue<KeyBind>().Active)
+                ExecuteR();
+        }
+
+
+        private static void ExecuteQ()
+        {
+            
+            Obj_AI_Hero target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            if (Q.IsReady() && ObjectManager.Player.Distance(target) <= Q.Range)
+            {
+                if (!Config.Item("Hitchance").GetValue<bool>())
+                    Q.Cast(target);
+                if (Config.Item("Hitchance").GetValue<bool>())
+                    Q.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
+                    //Q.Cast(target, false);
+            }
+ 
+        }
+        private static void ExecuteW()
+        {
+            Obj_AI_Hero target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            if (W.IsReady() && ObjectManager.Player.Distance(target) <= W.Range)
+            {
+                if (!Config.Item("Hitchance").GetValue<bool>())
+                    W.Cast(target);
+                if (Config.Item("Hitchance").GetValue<bool>())
+                    W.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
+            }
+        }
+        private static void ExecuteE()
+        {
+            Obj_AI_Hero target = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            if (E.IsReady() && ObjectManager.Player.Distance(target) <= E.Range)
+            {
+                if (!Config.Item("Hitchance").GetValue<bool>())
+                    E.Cast(target);
+                if (Config.Item("Hitchance").GetValue<bool>())
+                    E.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
+            }
+        }
+        private static void ExecuteR()
+        {
+            Obj_AI_Hero target = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            if (R.IsReady() && ObjectManager.Player.Distance(target) <= R.Range)
+            {
+                if (!Config.Item("Hitchance").GetValue<bool>())
+                    R.Cast(target);
+                if (Config.Item("Hitchance").GetValue<bool>())
+                    R.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
+            }
+        }
+    }
+}
---
+++ IHateSkillshots/SpellData.cs
@@ -0,0 +1,93 @@
+#region
+
+using System.Net.Configuration;
+using LeagueSharp;
+using LeagueSharp.Common;
+#endregion
+
+namespace Skillshots
+{
+    public class SpellData
+    {
+        public bool AddHitbox;
+        public string BaseSkinName;
+        public bool CanBeRemoved = false;
+        public float Delay;
+        public bool DonCross = false;
+        public int ExtraDuration;
+        public bool FixedRange;
+        public int Id = -1;
+        public float MissileSpeed;
+        public string MissileSpellName;
+        private float _range;
+        public SpellSlot Slot;
+        public string SpellName;
+        private float _radius;
+        public int DangerValue;
+        public string ToggleParticleName = "";
+        public string FromObject = "";
+        public Prediction.SkillshotType Type;
+        public bool DontAddExtraDuration;
+        public bool Centered;
+        public bool Invert;
+        public int MultipleNumber = -1;
+        public float MultipleAngle;
+        public int ExtraRange = -1;
+        public bool MissileFollowsUnit;
+
+        public bool DisableFowDetection = false;
+        public bool IsDangerous = false;
+
+        public int RingRadius;
+        public SpellData()
+        {
+
+        }
+
+        public SpellData(string baseSkinName, string spellName, SpellSlot slot, Prediction.SkillshotType type, float delay, float range,
+            float radius, float missileSpeed, bool addHitbox, bool fixedRange, int defaultDangerValue)
+        {
+            BaseSkinName = baseSkinName;
+            SpellName = spellName;
+            Slot = slot;
+            Type = type;
+            Delay = delay;
+            Range = range;
+            _radius = radius;
+            MissileSpeed = missileSpeed;
+            AddHitbox = addHitbox;
+            FixedRange = fixedRange;
+            DangerValue = defaultDangerValue;
+        }
+
+        public string MenuItemName
+        {
+            get { return BaseSkinName + " - " + SpellName; }
+        }
+
+        public float Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set { _radius = value; }
+        }
+
+
+
+        public float Range
+        {
+            get
+            {
+                return _range;
+            }
+            set
+            {
+                _range = value;
+            }
+        }
+
+
+    }
+}
