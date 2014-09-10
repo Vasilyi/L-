bb2f1b55930591ba1bb60badef7baf22999255be
trus <trus888@gmail.com>
2014-09-10 12:30:22 +0400

    Humanizer :^)

---
+++ Humanizer/Program.cs
@@ -0,0 +1,76 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+#endregion
+
+namespace Humanizer
+{
+    public class Program
+    {
+
+        public static Menu Config;
+
+
+        public class LatestCast
+        {
+            public static float Tick = 0;
+            public static float Timepass;
+            public static double X;
+            public static double Y;
+            public static double Distance;
+            public static double Delay;
+
+        }
+
+        private static Obj_AI_Hero Player;
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            Console.WriteLine("Humanizer LOADED");
+            LeagueSharp.Game.OnGameProcessPacket += PacketHandler;
+        }
+  
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Player = ObjectManager.Player;
+
+            //Create the menu
+            Config = new Menu("Humanizer", "Humanizer", true);
+
+            Config.AddSubMenu(new Menu("Config", "Config"));
+            Config.SubMenu("Config").AddItem(new MenuItem("delaytime", "Delay time")).SetValue(new Slider(0, 20, 0));
+           
+        }
+        private static void PacketHandler(GamePacketEventArgs args)
+        {
+            var Packetc = new GamePacket(args.PacketData);
+            if (Packetc.Header == Packet.C2S.Cast.Header)
+            {
+                var decodedpacket = Packet.C2S.Cast.Decoded(args.PacketData);
+                if (LatestCast.Tick + 500 < Environment.TickCount)
+                {
+                    LatestCast.Timepass = Environment.TickCount - LatestCast.Tick;
+                    LatestCast.Distance = Math.Sqrt(Math.Pow(decodedpacket.ToX - LatestCast.X, 2) + Math.Pow(decodedpacket.ToY - LatestCast.Y, 2));
+                    LatestCast.Delay = (LatestCast.Distance * 0.1 * Config.Item("delaytime").GetValue<Slider>().Value);
+                    if (Environment.TickCount > LatestCast.Tick + LatestCast.Delay)
+                    {
+                        args.Process = false;
+                    }
+                    if (args.Process == true)
+                    {
+                        LatestCast.Tick = Environment.TickCount;
+                        LatestCast.X = decodedpacket.ToX;
+                        LatestCast.Y = decodedpacket.ToY;
+                    }
+                }
+            }
+
+        }
+    }
+}
